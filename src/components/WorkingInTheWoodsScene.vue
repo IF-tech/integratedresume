ac1508562235479b39d6b293fedc7380183911f6
Ian Floyd <ianfloydtechnology@gmail.com>
2022-10-14 19:59:14 -0700

    added particles

--- src/components/WorkingInTheWoodsScene.vue
+++ src/components/WorkingInTheWoodsScene.vue
@@ -433,7 +433,56 @@ import Chart from './Chart.vue';
           }
           return { x: dx, y: dy };
         }
-  
+
+        let points;
+(function(){
+const particles = 20000;
+
+const geometry = new THREE.BufferGeometry();
+
+const positions = [];
+const colors = [];
+const color = new THREE.Color();
+
+const n = 500,
+  n2 = n / 2; // particles spread in the cube
+
+for (let i = 0; i < particles; i++) {
+  // positions
+
+  const x = Math.random() * n - n2;
+  const y = Math.random() * n - n2;
+  const z = Math.random() * n - n2;
+
+  positions.push(x, y, z);
+
+  // colors
+
+  const vx = 1;
+  const vy = 1;
+  const vz = 1;
+
+  color.setRGB(vx, vy, vz);
+
+  colors.push(color.r, color.g, color.b);
+}
+
+geometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(positions, 3)
+);
+geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+
+geometry.computeBoundingSphere();
+
+//
+
+const material = new THREE.PointsMaterial({ size: 1, vertexColors: true });
+
+points = new THREE.Points(geometry, material);
+
+})()
+scene.add(points);
         const clock = new THREE.Clock();
   
         const tick = () => {
@@ -476,6 +525,10 @@ import Chart from './Chart.vue';
           // Render
           renderer.render(scene, camera);
           // Call tick again on the next frame
+          const time = Date.now() * 0.001;
+
+    points.rotation.x = time * 0.025;
+    points.rotation.y = time * 0.05;
           requestAnimationFrame(tick);
         };
   
